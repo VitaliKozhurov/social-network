09ebecd6c849468e32e2e1d416562ee72646341e
VitaliKozhurov <vitalik24021992@gmail.com>
2023-05-16 23:18:13 +0300

    Fix index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -9,15 +9,12 @@ import {Provider} from 'react-redux';
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+root.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>
+);
 
-const rerenderEntireTree = () => {
-    root.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>
-    );
-};
-rerenderEntireTree();
-store.subscribe(rerenderEntireTree);
+
