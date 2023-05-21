9d50b8e80067629a946f91c4f799885a91f02ba4
VitaliKozhurov <vitalik24021992@gmail.com>
2023-05-21 22:29:29 +0300

    Add React.Component in App

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React from 'react';
 import {UserPageType} from '../../appTypes/types';
 import s from './Users.module.css';
 import avatar from '../../assets/image/userWithoutAvatar.webp';
@@ -11,70 +11,40 @@ type UsersPropsType = {
     setUsers: (users: Array<UserPageType>) => void
 }
 
-export const Users: FC<UsersPropsType> = ({users, followUser, unfollowUser, setUsers}) => {
-    /*!users.length && setUsers([
-        {
-            id: 1,
-            photoUrl: 'https://img.freepik.com/premium-vector/silhouette-japanese-samurai-warrior-with-sword-standing-sunset-art-print_194708-1868.jpg?w=2000',
-            fullName: 'Vitali Kozhurou',
-            status: 'Open to work',
-            followed: true,
-            location: {
-                city: 'Minsk',
-                country: 'Belarus'
-            },
-        },
-        {
-            id: 2,
-            photoUrl: 'https://img.freepik.com/premium-vector/silhouette-japanese-samurai-warrior-with-sword-standing-sunset-art-print_194708-1868.jpg?w=2000',
-            fullName: 'Max Roslow',
-            status: 'I am work now',
-            followed: false,
-            location: {
-                city: 'Moscow',
-                country: 'Russia'
-            },
-        },
-        {
-            id: 3,
-            photoUrl: 'https://img.freepik.com/premium-vector/silhouette-japanese-samurai-warrior-with-sword-standing-sunset-art-print_194708-1868.jpg?w=2000',
-            fullName: 'Ivan Petrov',
-            status: 'I am relocate',
-            followed: true,
-            location: {
-                city: 'Kiev',
-                country: 'Ukraine'
-            },
-        }
-    ])*/
-    if (!users.length) {
+export class Users extends React.Component<UsersPropsType> {
+
+    constructor(props: UsersPropsType) {
+        super(props);
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => setUsers(response.data.items))
+            .then(response => this.props.setUsers(response.data.items))
     }
 
+    render() {
+        const {users, followUser, unfollowUser} = this.props;
+        const usersList = users.map(user => {
+            const onFollowButtonHandler = () => {
+                user.followed && unfollowUser(user.id);
+                !user.followed && followUser(user.id);
+            }
 
-    const usersList = users.map(user => {
-        const onFollowButtonHandler = () => {
-            user.followed && unfollowUser(user.id);
-            !user.followed && followUser(user.id);
-        }
+            return (
+                <div key={user.id}>
+                    <h3>{user.name}</h3>
+                    <div className={s.avatar}>
+                        {user.photos.small ? <img src={user.photos.small} alt="Avatar" /> :
+                            <img src={avatar} alt="Avatar" />}
+                    </div>
+                    <p>{user.status}</p>
+                    <button onClick={onFollowButtonHandler}>{user.followed ? 'Unfollow' : 'Follow'}</button>
+                </div>
+            )
+        })
 
         return (
-            <div key={user.id}>
-                <h3>{user.name}</h3>
-                <div className={s.avatar}>
-                    {user.photos.small? <img src={user.photos.small} alt="Avatar" />:<img src={avatar} alt="Avatar" />}
-                </div>
-                <p>{user.status}</p>
-                <button onClick={onFollowButtonHandler}>{user.followed ? 'Unfollow' : 'Follow'}</button>
-            </div>
+            <>
+                <h2>Users list</h2>
+                {usersList}
+            </>
         )
-    })
-
-    return (
-        <>
-            <h2>Users list</h2>
-            {usersList}
-        </>
-    )
-};
+    }
+}
